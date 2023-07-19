bcbf6e061b4ebd553ba218b5143e1deb02666487
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-07-19 22:31:11 +0100

    Section Component added

---
+++ src/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Sections = styled.article`
+  background-color: ${({ theme }) => theme.color.background};
+  display: flex;
+  flex-direction: row;
+  padding: 36px;
+`;
