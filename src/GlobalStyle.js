9811abb3e226015340bbc37e6c1e390a51a6e164
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-07-19 22:28:14 +0100

    globalStyle added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+*,
+::after,
+::before {
+  box-sizing: inherit;
+}
+
+body {
+  background-color: ${({ theme }) => theme.color.background};
+  font-family: 'Inter', sans-serif;
+}
+
+`;
