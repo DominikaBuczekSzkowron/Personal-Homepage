f8d402d2eeb1cde1aa40d4e23fae17cb4ac7a593
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-07-20 22:07:11 +0100

    styled last section

--- src/styled.js
+++ src/styled.js
@@ -4,5 +4,19 @@ export const Sections = styled.article`
   background-color: ${({ theme }) => theme.color.background};
   display: flex;
   flex-direction: row;
+  justify-content: center;
   padding: 36px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CardContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr auto;
+  column-gap: 32px;
+  row-gap: 32px;
 `;
