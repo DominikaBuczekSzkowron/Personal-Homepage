7119bf3f4ddb42c73a3a7e5ac82558c398799489
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-07-20 22:09:08 +0100

    add main button, sections, cards and lists to body app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { Container } from "./components/Container";
 import { Section } from "./components/Section";
+import { MainButton } from "./components/MainButton";
 import { Image } from "./components/Image";
+import { PortfolioHeader } from "./components/PortfolioHeader";
+import { Card } from "./components/Card";
 import pic from "./picture.png";
-import { Sections } from "./styled";
+import { Sections, Wrapper, CardContainer } from "./styled";
 import { List } from "./components/List";
 import { Toggle } from "./components/Toggle";
 import { useEffect, useState } from "react";
@@ -13,6 +16,7 @@ import { lightTheme, darkTheme } from "./theme";
 export const App = () => {
   const [theme, setTheme] = useState("light");
   const [skills, setSkills] = useState([]);
+  const [wantedSkills, setWantedSkills] = useState([]);
 
   const toggleTheme = () => {
     if (theme === "light") {
@@ -26,7 +30,13 @@ export const App = () => {
     fetch("skills.json")
       .then((response) => response.json())
       .then((skills) => setSkills(skills));
-  });
+  }, []);
+
+  useEffect(() => {
+    fetch("wantedSkills.json")
+      .then((response) => response.json())
+      .then((wantedSkills) => setWantedSkills(wantedSkills));
+  }, []);
 
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
@@ -39,16 +49,62 @@ export const App = () => {
             onClick={toggleTheme}
           />
           <Sections>
-            <>
-              <Image src={pic} alt="Dominika" />
+            <Image src={pic} alt="Dominika" />
+            <Wrapper>
               <Section
                 text="THIS IS"
                 title="Dominika Buczek-Szkowron"
                 content="I’m a passionate Frontend Developer in love with React,currently looking for new job opportunities."
               ></Section>
-            </>
+              <MainButton
+                email="dominikabuczek@gmail.com"
+                subject="JOB"
+                body="You are great! We want you on board!"
+              />
+            </Wrapper>
           </Sections>
           <List title="My skill includes" data={skills}></List>
+          <List title="What I want to learn next" data={wantedSkills}></List>
+
+          <Sections>
+            <Wrapper>
+              <PortfolioHeader
+                title="Portfolio"
+                subtitle="My resent projects"
+              />
+              <CardContainer>
+                <Card
+                  title="Movies Browser"
+                  content="Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search."
+                  demoLink="https://link.com"
+                  codeLink="https://code.com"
+                ></Card>
+                <Card
+                  title="Movies Browser"
+                  content="Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search."
+                  demoLink="https://link.com"
+                  codeLink="https://code.com"
+                ></Card>
+                <Card
+                  title="Movies Browser"
+                  content="Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search."
+                  demoLink="https://link.com"
+                  codeLink="https://code.com"
+                ></Card>
+                <Card
+                  title="Movies Browser"
+                  content="Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search."
+                  demoLink="https://link.com"
+                  codeLink="https://code.com"
+                ></Card>
+              </CardContainer>
+            </Wrapper>
+          </Sections>
+          <Section
+            text="LET'S TALK!"
+            title="dominikabuczekszkowron@gmail.com"
+            content="I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to conatct me"
+          ></Section>
         </Container>
       </>
     </ThemeProvider>
