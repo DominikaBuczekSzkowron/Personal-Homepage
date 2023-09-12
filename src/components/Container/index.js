0d3e2c3fafa565bd157165c5e5a22890b44d5eec
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-09-12 22:43:17 +0100

    extra padding added

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -4,4 +4,13 @@ export const Container = styled.div`
   max-width: 1216px;
   margin: auto;
   word-break: break-word;
+  padding: 36px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.ipod}) {
+    padding: 24px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    padding: 12px;
+  }
 `;
